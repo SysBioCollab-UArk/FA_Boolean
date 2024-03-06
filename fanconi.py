56f4e81a817f810aaa9c1ab6234e51d057942da2
lh64 <lh64@cornell.edu>
2024-03-06 09:59:39 -0600

    Added a function to util.py to run an asynchronous updating BooleanNet simulation and plot the results. We can now compare the synchronous and asynchronous results against each other.

--- fanconi.py
+++ fanconi.py
@@ -1,5 +1,4 @@
-import boolean2
-from util import create_heatmap
+from util import *
 
 # construct the model
 Initial_conditions = '''
@@ -31,34 +30,34 @@ CHKREC = False
 '''
 
 Rules = '''
-ICL* = ICL and not DSB
-FANCM* = ICL and not CHKREC
-FAcore* = FANCM and (ATR or ATM) and not CHKREC
-FANCD2I* = FAcore and ((ATM or ATR) or (H2AX and DSB)) and not USP1
-MUS81* = ICL
-FANCJBRCA1* = (ICL or ssDNARPA) and (ATM or ATR)
-XPF* = (MUS81 and not FANCM) or (MUS81 and p53 and not (FAcore and FANCD2I and FAN1))
-FAN1* = MUS81 and FANCD2I
-ADD* = (ADD or (MUS81 and (FAN1 or XPF))) and not PCNATLS
-DSB* = (DSB or FAN1 or XPF) and not (NHEJ or HRR)
-PCNATLS* = (ADD or (ADD and FAcore)) and not (USP1 or FAN1)
-MRN* = DSB and ATM and not ((KU and FANCD2I) or RAD51 or CHKREC)
-BRCA1* = DSB and (ATM or CHK2 or ATR) and not CHKREC
-ssDNARPA* = DSB and ((FANCD2I and FANCJBRCA1) or MRN) and not (RAD51 or KU)
-FANCD1N* = (ssDNARPA and BRCA1) or (FANCD2I and ssDNARPA) and not CHKREC
-RAD51* = ssDNARPA and FANCD1N and not CHKREC
-HRR* = DSB and RAD51 and FANCD1N and BRCA1 and not CHKREC
-USP1* = ((FANCD1N and FANCD2I) or PCNATLS) and not FANCM
-KU* = DSB and not (MRN or FANCD2I or CHKREC)
-DNAPK* = (DSB and KU) and not CHKREC
-NHEJ* = (DSB and DNAPK and XPF and not ((FANCJBRCA1 and ssDNARPA) or CHKREC)) or ((DSB and DNAPK and KU) and not (ATM and ATR))
-ATR* = (ssDNARPA or FANCM or ATM) and not CHKREC
-ATM* = (ATR or DSB) and not CHKREC
-p53* = (((ATM and CHK2) or (ATR and CHK1)) or DNAPK) and not CHKREC
-CHK1* = (ATM or ATR or DNAPK) and not CHKREC
-CHK2* = (ATM or ATR or DNAPK) and not CHKREC
-H2AX* = DSB and (ATM or ATR or DNAPK) and not CHKREC
-CHKREC* = ((PCNATLS or NHEJ or HRR) and not DSB) or (not ADD and not ICL and not DSB and not CHKREC)
+1: ICL* = ICL and not DSB
+1: FANCM* = ICL and not CHKREC
+1: FAcore* = FANCM and (ATR or ATM) and not CHKREC
+1: FANCD2I* = FAcore and ((ATM or ATR) or (H2AX and DSB)) and not USP1
+1: MUS81* = ICL
+1: FANCJBRCA1* = (ICL or ssDNARPA) and (ATM or ATR)
+1: XPF* = (MUS81 and not FANCM) or (MUS81 and p53 and not (FAcore and FANCD2I and FAN1))
+1: FAN1* = MUS81 and FANCD2I
+1: ADD* = (ADD or (MUS81 and (FAN1 or XPF))) and not PCNATLS
+1: DSB* = (DSB or FAN1 or XPF) and not (NHEJ or HRR)
+1: PCNATLS* = (ADD or (ADD and FAcore)) and not (USP1 or FAN1)
+1: MRN* = DSB and ATM and not ((KU and FANCD2I) or RAD51 or CHKREC)
+1: BRCA1* = DSB and (ATM or CHK2 or ATR) and not CHKREC
+1: ssDNARPA* = DSB and ((FANCD2I and FANCJBRCA1) or MRN) and not (RAD51 or KU)
+1: FANCD1N* = (ssDNARPA and BRCA1) or (FANCD2I and ssDNARPA) and not CHKREC
+1: RAD51* = ssDNARPA and FANCD1N and not CHKREC
+1: HRR* = DSB and RAD51 and FANCD1N and BRCA1 and not CHKREC
+1: USP1* = ((FANCD1N and FANCD2I) or PCNATLS) and not FANCM
+1: KU* = DSB and not (MRN or FANCD2I or CHKREC)
+1: DNAPK* = (DSB and KU) and not CHKREC
+1: NHEJ* = (DSB and DNAPK and XPF and not ((FANCJBRCA1 and ssDNARPA) or CHKREC)) or ((DSB and DNAPK and KU) and not (ATM and ATR))
+1: ATR* = (ssDNARPA or FANCM or ATM) and not CHKREC
+1: ATM* = (ATR or DSB) and not CHKREC
+1: p53* = (((ATM and CHK2) or (ATR and CHK1)) or DNAPK) and not CHKREC
+1: CHK1* = (ATM or ATR or DNAPK) and not CHKREC
+1: CHK2* = (ATM or ATR or DNAPK) and not CHKREC
+1: H2AX* = DSB and (ATM or ATR or DNAPK) and not CHKREC
+1: CHKREC* = ((PCNATLS or NHEJ or HRR) and not DSB) or (not ADD and not ICL and not DSB and not CHKREC)
 '''
 # CHKREC* = ((PCNATLS or NHEJ or HRR) and not DSB) or ((not ADD) and (not ICL) and (not DSB) and not CHKREC)
 
@@ -73,27 +72,13 @@ for i in range(3):  # TODO: we should generalize this
     else:
         model_text = Initial_conditions + 'DSB=False\nADD=False\nICL=True\n' + Rules
 
-    # create the BooleanNet model and simulate it
+    # run a synchronous updating simulation of the BooleanNet model
     n_iterations = 20
-    model = boolean2.Model(model_text, mode='sync')
-    model.initialize()
-    model.iterate(steps=n_iterations)
+    run_FA_Boolean_synch(model_text, n_iterations)
 
-    # get the names of the nodes and get the indices so we can order them the same as in Rodriguez et al. (2012)
-    nodes = model.states[0].keys()
-    nodes_order = ['ICL', 'FANCM', 'FAcore', 'FANCD2I', 'MUS81', 'FANCJBRCA1', 'XPF', 'FAN1', 'ADD', 'DSB', 'PCNATLS',
-                   'MRN', 'BRCA1', 'ssDNARPA', 'FANCD1N', 'RAD51', 'HRR', 'USP1', 'KU', 'DNAPK', 'NHEJ', 'ATR', 'ATM',
-                   'p53', 'CHK1', 'CHK2', 'H2AX', 'CHKREC']
-    index = []
-    for node in nodes_order:
-        index.append(nodes.index(node))
+    # run an asynchronous updating simulation of the BooleanNet model
+    n_iterations = 20
+    n_runs = 100
+    run_FA_Boolean_asynch(model_text, n_iterations, n_runs)
 
-    # collect the state values for each iteration
-    states = []
-    for state in model.states:
-        # states.append([int(x) for x in state.values()])
-        states.append([int(state.values()[index[i]]) for i in range(len(nodes))])  # ordered as Rodriguez et al. (2012)
-
-    # create the heatmap for this initial condition
-    # create_heatmap(states, nodes)
-    create_heatmap(states, nodes_order)
+    plt.show()
--- util.py
+++ util.py
@@ -1,6 +1,62 @@
+import boolean2
+from boolean2 import util
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+
+
+def run_FA_Boolean_synch(model_text, n_sim_steps):
+    model = boolean2.Model(model_text, mode='sync')
+    model.initialize()
+    model.iterate(steps=n_sim_steps)
+
+    # get the names of the nodes and get the indices so we can order them the same as in Rodriguez et al. (2012)
+    nodes = model.states[0].keys()
+    nodes_order = ['ICL', 'FANCM', 'FAcore', 'FANCD2I', 'MUS81', 'FANCJBRCA1', 'XPF', 'FAN1', 'ADD', 'DSB', 'PCNATLS',
+                   'MRN', 'BRCA1', 'ssDNARPA', 'FANCD1N', 'RAD51', 'HRR', 'USP1', 'KU', 'DNAPK', 'NHEJ', 'ATR', 'ATM',
+                   'p53', 'CHK1', 'CHK2', 'H2AX', 'CHKREC']
+    index = []
+    for node in nodes_order:
+        index.append(nodes.index(node))
+
+    # collect the state values for each iteration
+    states = []
+    for state in model.states:
+        # states.append([int(x) for x in state.values()])
+        states.append([int(state.values()[index[i]]) for i in range(len(nodes))])  # ordered as Rodriguez et al. (2012)
+
+    # create the heatmap for this initial condition
+    # create_heatmap(states, nodes)
+    create_heatmap(states, nodes_order)
+
+
+def run_FA_Boolean_asynch(model_text, n_sim_steps, n_runs):
+    # for storing trajectories
+    coll = util.Collector()
+
+    # run the simulations
+    for i in range(n_runs):
+        print(i)
+
+        model = boolean2.Model(model_text, mode='async')
+        model.initialize()
+        model.iterate(steps=n_sim_steps)
+        coll.collect(states=model.states, nodes=model.nodes)
+
+    # get average node values
+    avgs = coll.get_averages(normalize=True)
+
+    # plots
+    plt.figure(figsize=(6.4*1.5, 4.8*1.5))  # (6.4, 4.8)
+    for sp in sorted(avgs.keys()):
+        plt.plot(avgs[sp], lw=2, label=sp)
+    plt.xticks(ticks=np.arange(0, n_sim_steps+1, 2))
+    plt.ylim((-0.1, 1.1))
+    plt.xlabel('iteration')
+    plt.ylabel('probability ON')
+    plt.legend(ncol=1, bbox_to_anchor=[1, 1], loc=0, labelspacing=0.2)
+    # defaults: labelspacing=0.5, columnspacing=2)
+    plt.tight_layout()
 
 
 def create_heatmap(data, xtick_labels):
@@ -16,5 +72,3 @@ def create_heatmap(data, xtick_labels):
 
     res.set_yticklabels(np.arange(1, len(data)+1), rotation=0)
     ax.set_ylabel('Iteration')
-
-    plt.show()
